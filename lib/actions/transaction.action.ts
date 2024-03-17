6e60ed28f91b439ac1d17e545853a5fc331a8873
suyashpurwar1 <suyashpurwar1@gmail.com>
2024-03-17 16:18:47 +0530

    update transaction action

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import Stripe from "stripe";
-import { handleError } from '../utils';
-import { connectToDatabase } from '../database/mongoose';
-import Transaction from '../database/models/transaction.model';
-import { updateCredits } from './user.actions';
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database/mongoose";
+import Transaction from "../database/models/transaction.model";
+import { updateCredits } from "./user.actions";
 
 export async function checkoutCredits(transaction: CheckoutTransactionParams) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -16,30 +16,26 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     line_items: [
       {
         price_data: {
-<<<<<<< HEAD
-          currency: 'inr',
-=======
-          currency: 'usd',
->>>>>>> 55bb397df140a3e0776c695ee716bc8b72d9cbd4
+          currency: "inr",
           unit_amount: amount,
           product_data: {
             name: transaction.plan,
-          }
+          },
         },
-        quantity: 1
-      }
+        quantity: 1,
+      },
     ],
     metadata: {
       plan: transaction.plan,
       credits: transaction.credits,
       buyerId: transaction.buyerId,
     },
-    mode: 'payment',
+    mode: "payment",
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+  });
 
-  redirect(session.url!)
+  redirect(session.url!);
 }
 
 export async function createTransaction(transaction: CreateTransactionParams) {
@@ -48,13 +44,14 @@ export async function createTransaction(transaction: CreateTransactionParams) {
 
     // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
-      ...transaction, buyer: transaction.buyerId
-    })
+      ...transaction,
+      buyer: transaction.buyerId,
+    });
 
     await updateCredits(transaction.buyerId, transaction.credits);
 
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
